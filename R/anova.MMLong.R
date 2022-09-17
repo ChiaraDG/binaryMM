cfc747be8008bf6794aa40d6e772ba7baa399f60
Chiara Di Gravio <chiara.di.gravio@vanderbilt.edu>
2022-09-17 15:25:11 -0500

    add example in ANOVA function

--- R/anova.MMLong.R
+++ R/anova.MMLong.R
@@ -1,27 +1,26 @@
 #' Comparing Two Models: ANOVA
 #'
 #' Compute analysis of variance tables for two fitted model objects
-#' @param object a list with two elements. Element 1 is the first model fit using \code{mm}, element 2 is the second model fit using \code{mm}
-#' @param ... additional control variables
+#' @param object model resulting from \code{mm}
+#' @param ...  model resulting from \code{mm} Note that \code{anova} only allow users to compare two models
 #'
 #' @return ANOVA table
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' data(datrand)
+#' fit1 <- mm(Y~time*binary, t.formula=~1, data=datrand, id=id, step.max=4, verbose=FALSE)
+#' fit2 <- mm(Y~time*binary, t.formula=~1, lv.formula =~1, data=datrand, id=id, step.max=4, verbose=FALSE)
+#' anova(fit1,fit2)}
+#'
 anova.MMLong <-
 function(object, ...) {
   oo1 = object
   oo2 = list(...)[[1]]
 
-  #mod1 = list(mf=oo1$m.formula, tf=oo1$t.formula, lvf=oo1$lv.formula)
-  #mod2 = list(mf=oo2$m.formula, tf=oo2$t.formula, lvf=oo2$lv.formula)
-
   mod1 = list(mf=oo1$call)
   mod2 = list(mf=oo2$call)
-
-  #if(!identical(mod1[2:3],mod2[2:3])) {
-  #  print('Warning: Association models differ!')
-  #}
 
   info.mat = rbind(oo1$info_stats, oo2$info_stats)
   info.mat = data.frame(cbind(df=c(nrow(oo1$mod.cov),nrow(oo2$mod.cov)), info.mat))
